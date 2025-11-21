dac88536105d5cdcdb1703f9f98cbd718e1ef87f
Tuankien111 <lamminhtuankien@gmail.com>
2025-11-21 14:50:33 +0700

    Update: change title

--- resources/views/test-interface.blade.php
+++ resources/views/test-interface.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test Mini App API (Mock Mode)</title>
+    <title>Test Mini App API (Mock Mode) Tuấn Kiện</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         /* Custom scrollbar for aesthetics */
